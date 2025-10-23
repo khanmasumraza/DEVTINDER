9c09aec4c457e0e1069a22d4a4ada155d1086a66
Khanmasum123 <bcamasum@gmail.com>
2025-10-23 15:30:01 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,9 @@ const express=require("express")
 
 const app= express();
 
-app.use("/user",(req,res)=>{
-    res.send("lol")
-})
-
-
-// This will only handle GET call to /user
-app.get("/user",(req,res)=>{
+app.get("/user/:userId",(req,res)=>{
+    console.log(req.params);
     res.send({firstname:"Masum",lastname:"Raza"})
-})
-
-app.post("/user",(req,res)=>{
-    // Saving data to DB
-    res.send("Data successfully save to database")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted succesfully")
-})
-
-// This method match all the HTTP method API calls to /test
-app.use("/contact",(req,res)=>{
-    res.send("This is my contact page")
 })
 
 app.listen(3000);
