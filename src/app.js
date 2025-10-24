bbbfc776b8722d9c4ed0b05987339f5158dbc7dc
Khanmasum123 <bcamasum@gmail.com>
2025-10-24 11:28:17 +0530

    mutiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
 const express=require("express")
 
-const app= express();
+const app=express();
 
-app.get("/user/:userId",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"Masum",lastname:"Raza"})
-})
+// app.use("/rputer",rh1,rh2,[rh3],rh4,rh5)
 
-app.listen(3000);
+app.use("/user",[(req,res,next)=>{
+console.log("This is route handler 1")
+next();
+// res.send(" Response")
+},
+
+(req,res,next) =>{
+  console.log("Route handling 2 ")
+  // res.send("Response 2")
+  next();
+},
+(req,res,next) =>{
+  console.log("Route handling 3 ")
+  // res.send("Response 3")
+  next();
+},
+
+(req,res,next) =>{
+  console.log("Route handling 4 ")
+  // res.send("Response 4")
+  next();
+},
+(req,res,next) =>{
+  console.log("Route handling 5 ")
+  res.send("Response 5")
+}
+]);
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777...");
+});
