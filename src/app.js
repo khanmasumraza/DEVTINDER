481f407fd0df71553470e6a70fa992af72ccdff9
Khanmasum123 <bcamasum@gmail.com>
2025-10-30 22:11:51 +0530

    patch del api

--- src/app.js
+++ src/app.js
@@ -54,6 +54,34 @@ const users= await User.find({})
    res.status(404).send("Something went wrong"); 
   }
 })
+
+// delete a user from the database
+
+app.delete("/user",async (req,res)=>{
+  const userId=req.body.userId;
+  try{
+   const user= await User.findByIdAndDelete(userId)
+   res.send("User delete succesfully")
+  }
+  catch (err){
+    res.status(404).send("Something went wrong"); 
+  }
+})
+
+// update a data to database
+
+app.patch("/user",async(req,res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+  try{
+const user=await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after"})
+console.log(user)
+res.send("User updated succesfully")
+  }
+  catch (err){
+      res.status(404).send("Something went wrong"); 
+  }
+})
 dbConnect()
 
 .then(()=>{
